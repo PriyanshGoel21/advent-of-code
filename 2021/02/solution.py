b65e4d3ff77a5a804d1adb9467e84174f90ee793
Priyansh Goel <Priyanshgoel05@gmail.com>
2021-12-02 10:58:45 +0530

    Added 2021 Day 2 Solution

---
+++ 2021/02/solution.py
@@ -0,0 +1,54 @@
+def parse_data():
+    with open("input.txt", "r") as file:
+        data = [line.strip() for line in file]
+    return data
+
+
+def part_one(data: list[str]):
+    forward = 0
+    depth = 0
+    for command in data:
+        com, x = command.split()
+        match com:
+            case "forward":
+                forward += int(x)
+            case "down":
+                depth += int(x)
+            case "up":
+                depth -= int(x)
+    return forward * depth
+
+
+def part_two(data: list[str]):
+    forward = 0
+    depth = 0
+    aim = 0
+    for command in data:
+        com, x = command.split()
+        match com:
+            case "forward":
+                forward += int(x)
+                depth += aim * int(x)
+            case "down":
+                aim += int(x)
+            case "up":
+                aim -= int(x)
+    return forward * depth
+
+
+def part_one_esoteric(data):
+    ...
+
+
+def part_two_esoteric(data):
+    ...
+
+
+def main():
+    data = parse_data()
+    print(part_one(data))
+    print(part_two(data))
+
+
+if __name__ == "__main__":
+    main()
