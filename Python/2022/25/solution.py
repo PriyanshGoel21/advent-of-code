d66e72c99e886f61e8b497c588d35b44d3a21f4f
Priyansh Goel <priyanshgoel05@gmail.com>
2022-01-02 15:34:00 +0530

    day 14-25

---
+++ Python/2022/25/solution.py
@@ -0,0 +1,21 @@
+DIGITS = {"2": 2, "1": 1, "0": 0, "-": -1, "=": -2}
+DIGITS_R = {v: k for k, v in DIGITS.items()}
+
+
+def p1(f):
+    total = 0
+
+    for num in f:
+        for i, digit in enumerate(num.strip()[::-1]):
+            total += DIGITS[digit] * 5**i
+
+    ans = ""
+
+    while total > 0:
+        total, digit = divmod(total, 5)
+        if digit > 2:
+            digit -= 5
+            total += 1
+        ans += DIGITS_R[digit]
+
+    return ans[::-1]
