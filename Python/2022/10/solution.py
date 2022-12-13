b93381a92385e62509d65567776a49f24f1bd390
Priyansh Goel <priyanshgoel05@gmail.com>
2022-12-13 15:33:28 +0530

    day 8,9,10,11,12,13

---
+++ Python/2022/10/solution.py
@@ -0,0 +1,47 @@
+def p1(f):
+    X = 1
+    t = 0
+    signal = 0
+
+    def tick():
+        nonlocal signal, t
+        t += 1
+        if t in (20, 60, 100, 140, 180, 220):
+            signal += t * X
+
+    for line in f:
+        match line.split():
+            case ["addx", num]:
+                tick()
+                tick()
+                X += int(num)
+            case ["noop"]:
+                tick()
+
+    return signal
+
+
+def p2(f):
+    X = 1
+    t = 0
+
+    print()
+
+    def tick():
+        nonlocal t
+        if t % 40 in (X - 1, X, X + 1):
+            print("#", end="")
+        else:
+            print(".", end="")
+        t += 1
+        if t % 40 == 0:
+            print()
+
+    for line in f:
+        match line.split():
+            case ["addx", num]:
+                tick()
+                tick()
+                X += int(num)
+            case ["noop"]:
+                tick()
